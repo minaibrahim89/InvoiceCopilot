f0e16fd5117cd7bf6f46ca7d6c35eaf399e96604
Mina Ibrahim <iar1fe@bosch.com>
2026-02-04 13:55:32 +0100

    Increase sleep duration to 10 seconds in render script for Structurizr container

--- architecture/render.ps1
+++ architecture/render.ps1
@@ -1,4 +1,4 @@
 $PATH = Split-Path -Parent $MyInvocation.MyCommand.Path
 docker run -d --rm --name invoice-copilot-structurizr -p 8080:8080 -v "${PATH}:/usr/local/structurizr" structurizr/lite
-Sleep 5
+Sleep 10
 Start-Process "http://localhost:8080"
